e7ba8fae3ec6d736e67ea597017372548e9b68b6
meloactive <meloactive@re-gister.com>
2021-11-14 20:06:48 +0600

    simple list in django

--- os_videos_list/views.py
+++ os_videos_list/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def demo_view(request):
-    return render(request, 'os_videos_list/demo.html')
+    test_arr = []
+    for i in range(10):
+        test_arr.append(i)
+    return render(request, 'os_videos_list/demo.html', {"lists": test_arr})
